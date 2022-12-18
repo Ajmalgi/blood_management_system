9762a45c67241347bbdd6bc4aaa94a492ba80fd9
Ajmal# <ajmalomr5@gmail.com>
2022-12-18 11:20:51 +0530

    patient template before setting master page'

---
+++ blood_management_system/patient/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+app_name = 'patient_app'
+urlpatterns = [
+    path('', views.patient_index,name='index'),
+    path('about',views.about,name='about'),
+    path('blog',views.blog,name='blog'),
+    path('contact',views.contact,name='contact'),
+  
+
+    
+   
+   
+]
---
+++ blood_management_system/patient/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def patient_index(request):
+    return render(request,'patient_templates/index.html')
+
+def about(request):
+    return render(request,'patient_templates/about.html')
+
+def blog(request):
+    return render(request,'patient_templates/blog.html')
+
+def contact(request):
+    return render(request,'patient_templates/contact.html')
+
